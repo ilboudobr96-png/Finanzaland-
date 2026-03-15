3232e81e8d3b5765f399c1667763fd13088431b4
ilboudobr96-png <ilboudobr96@gmail.com>
2026-03-15 23:47:03 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+// FinanzaLand Service Worker v11
+const CACHE_NAME = 'finanzaland-v11';
+const ASSETS = ['/', '/indice.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Network-first: sempre versione fresca
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
